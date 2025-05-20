762085e87c826c6be752b4f82d80e95ca443b2a5
Andrew Pritchine <sedartes@mail.ru>
2025-05-20 04:10:07 +0300

    sr

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,8 +111,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/django')
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
