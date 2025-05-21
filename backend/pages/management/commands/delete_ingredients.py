9b03dd107999d2a13d51684ac9d8087d95b185be
Andrew Pritchine <sedartes@mail.ru>
2025-05-21 03:19:50 +0300

    last commit

---
+++ backend/pages/management/commands/delete_ingredients.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = "Удаляет все ингредиенты из базы данных"
+
+    def handle(self, *args, **kwargs):
+        count = Ingredient.objects.count()
+        Ingredient.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS(f"Удалено ингредиентов: {count}"))
