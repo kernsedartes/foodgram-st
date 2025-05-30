6eaa419d4397562550545e15a9b7cbc32c2b9024
Andrew Pritchine <sedartes@mail.ru>
2025-05-31 00:21:40 +0300

    frfr

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,7 +4,6 @@ from .models import (
     Recipe,
     Favorite,
     Ingredient,
-    Tag,
     RecipeIngredient,
     ShoppingCart,
 )
@@ -30,12 +29,6 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ("name", "measurement_unit")
     search_fields = ("name",)
     list_filter = ("name",)
-    empty_value_display = "-пусто-"
-
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ("name", "color", "slug")
     empty_value_display = "-пусто-"
 
 
