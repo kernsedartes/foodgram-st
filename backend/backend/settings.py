c18dc41ec31dbf002a45671bf08fe8fa2993299d
Andrew Pritchine <sedartes@mail.ru>
2025-05-31 00:50:36 +0300

    frfr

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(Path(__file__).resolve().parents[2] / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
